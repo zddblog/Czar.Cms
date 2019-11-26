a1aaf6f68493d340d2154759e4ad803a2634d8f5
zddblog <bosszddquan@163.com>
2019-11-26 12:29:45 +0800

    Signed-off-by: zddblog <bosszddquan@163.com>分支

---
+++ Sample02/Core2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Core2._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+              .ConfigureAppConfiguration((hostingContext, config) =>
+              {
+                  var env = hostingContext.HostingEnvironment;
+
+                  config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                        .AddEnvironmentVariables();
+
+              })
+                .UseStartup<Startup>();
+    }
+}
